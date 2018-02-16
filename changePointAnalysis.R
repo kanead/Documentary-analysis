6b9b0a9d53d561bc28125819676a8d224e24175c
Adam Kane <kanead@tcd.ie>
2018-02-16 17:16:01 +0000

    medians for time series

--- changePointAnalysis.R
+++ changePointAnalysis.R
@@ -3,27 +3,104 @@ rm(list=ls())
 # https://rpubs.com/richkt/269908
 
 library(pageviews)
-library(changepoint)
+# library(changepoint)
 library(dplyr)
-library(pageviews)
-library(data.table)
+# library(data.table)
+
+# USA airdates 
+# 18th Feb, 25th Feb, 4th March, 11th March, 18th March, 25th March 
+# UK airdates 
+# 6th Nov, 13th Nov, 20th Nov, 27th Nov, 4th Dec, 11th Dec
 
 # load in the data which is a vector of species names 
 setwd("C:\\Users\\akane\\Desktop\\Science\\Manuscripts\\Documentary analysis\\Documentary-analysis")
 data<-read.csv("mentionedNames.csv", header = TRUE, sep = ",")
 
+# articles <- c("Rugby_union","Football") # test using these articles 
+articles <- data$name
 
-articles <- c("Rugby_union", "Football")
-
-blah <- function(x){article_pageviews(project = "en.wikipedia", article = x
-                                          , start = as.Date('2017-01-01'), end = as.Date("2017-12-31")
+# the dates before airing 
+before <- function(x){article_pageviews(project = "en.wikipedia", article = x
+                                          , start = as.Date('2016-05-01'), end = as.Date("2016-10-31")
                                           , user_type = "user", platform = c("mobile-web"))
 }
 
-output<-articles %>% blah
+# the dates after airing 
+after <- function(x){article_pageviews(project = "en.wikipedia", article = x
+                                        , start = as.Date('2017-05-01'), end = as.Date("2017-10-31")
+                                        , user_type = "user", platform = c("mobile-web"))
+}
 
-dat.grp %>% 
+# collect the wiki hits for before 
+outputBefore <-articles %>% before
+# collect the wiki hits for after 
+outputAfter <-articles %>% after
+
+# split the dataset by article and find the median wiki hits for each before the date
+splitArticlesBefore<-with(outputBefore, tapply(views, outputBefore$article, median))
+#splitArticlesBefore<-data.frame(splitArticlesBefore)
+
+# split the dataset by article and find the median wiki hits for each 
+splitArticlesAfter<-with(outputAfter, tapply(views, outputAfter$article, median))
+#splitArticlesAfter<-data.frame(splitArticlesAfter)
+
+# combine the arrays 
+combinedHits <- cbind(splitArticlesBefore, splitArticlesAfter)
+combinedHits<-data.frame(combinedHits)
+
+# which one is bigger? Before or after? 
+combinedHits$change<-ifelse(combinedHits$splitArticlesAfter>combinedHits$splitArticlesBefore,1,0)
+combinedHits
+sum(combinedHits$change) /length(combinedHits$change)
+
+
+combinedHits <- merge(splitArticlesBefore,splitArticlesAfter)
+
+splitArticles$change<-ifelse(splitArticles$TRUE.>splitArticles$FALSE.,1,0)
+splitArticles
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+output$date <- as.Date(output$date)
+ls<- split(output,output$date<as.Date("2017-06-01"))
+
+ls
+
+splitData<-output %>% group_by(article) %>%
+  
+  
+
+
+output %>% 
+group_by(article) %>% 
+do(as.data.frame(cpt.mean(as.numeric(as.character(output$views)), method="BinSeg",Q=2)))
+
+
+test
+
+
+
+
+
+
+
+
+
+output %>% 
   select(article, views) %>% 
+  mean(views)
   do(cpt.mean(.)) 
 
 class(dat.grp$views)
@@ -70,12 +147,17 @@ cpts.ts(dis.pelt)
 class(discoveries)
 
 
-tsData<-article_pageviews(project = "en.wikipedia", article = "Golden_mole"
+tsData<-article_pageviews(project = "en.wikipedia", article = "Rugby_union"
                                          , start = as.Date('2016-01-01'), end = as.Date("2017-12-31")
                                          , user_type = "user", platform = c("desktop"))
 
 tsData<-(tsData[8])
 tsData<-as.numeric(as.character(tsData$views))
+mvalue = cpt.mean(tsData, method="BinSeg",Q=2)
+cpts(mvalue)
+plot(mvalue)
+
+
 
 # tsData<-as.ts(tsData)
 m.pelt <- cpt.mean(tsData, method = "PELT")
